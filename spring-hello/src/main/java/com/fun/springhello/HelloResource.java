9cbb76b23f87737e7e452d158c1779f74d32f93d
Vinicius Oliveira <vini2208@gmail.com>
2020-11-02 11:04:14 -0200

    Initial import.

---
+++ spring-hello/src/main/java/com/fun/springhello/HelloResource.java
@@ -0,0 +1,14 @@
+package com.fun.springhello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+
+	@GetMapping(path = "hello")
+	public String hello() {
+		return "Hello world!";
+	}
+
+}
